a30ebbf8c36c3d6274c26d7f898cf3b053bd9f3f
ElisendaGascon <elisendagascon@icloud.com>
2022-12-01 11:12:00 +0000

    Add project files.

---
+++ SolutionFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using advent_of_code_2022.Solutions;
+
+public static class SolutionFactory
+{
+    public static ISolution GetSolution(int day, int part)
+    {
+        string className = $"AoC2018.Solutions.Day{day:D2}.Part{part:D2}";
+        Type targetType = typeof(ISolution).Assembly.GetType(className);
+        var instance = (ISolution)Activator.CreateInstance(targetType);
+
+        return instance;
+    }
+}
---
+++ Solutions/ISolution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code_2022.Solutions
+{
+    public interface ISolution
+    {
+        string Solve(string input);
+    }
+}
