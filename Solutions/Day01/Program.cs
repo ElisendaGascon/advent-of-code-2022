d9e795ac6c9041ee82efe0181ecc52c8fe6d9d87
ElisendaGascon <elisendagascon@icloud.com>
2022-12-01 15:20:29 +0000

    Replaced `\n` with `Environment.NewLine`

--- Solutions/Day01/Program.cs
+++ Solutions/Day01/Program.cs
@@ -5,7 +5,7 @@ string data = File.ReadAllText(path);
 
 // Console.WriteLine(data);
 
-int result = data
+/*int result = data
     .Split("\n\n")
     .Select(x => x
         .Split("\n", StringSplitOptions.RemoveEmptyEntries)
@@ -13,9 +13,17 @@ int result = data
         .Aggregate((a, b) => a + b))
     .Max();
 
+Console.WriteLine(result);*/
+
+int result = data
+    .Split(Environment.NewLine + Environment.NewLine)
+    .Select(x => x
+        .Split(Environment.NewLine)
+        .Select(x => int.Parse(x))
+        .Aggregate((a, b) => a + b))
+    .Max();
+
 Console.WriteLine(result);
-
-
 
 /*
 string initialString = "1\n2\n3\n4\n\n5\n6";
