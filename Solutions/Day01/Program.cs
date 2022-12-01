67817f4b1f40c8b9d82c9457ae694116bd9bd47c
ElisendaGascon <elisendagascon@icloud.com>
2022-12-01 15:04:56 +0000

    Add solution (not working)

--- Solutions/Day01/Program.cs
+++ Solutions/Day01/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string path = @"c:\Users\ElisendaGascon\repos\advent-of-code-2022\Input\Day01.txt";
+string data = File.ReadAllText(path);
+
+var result = data
+    .Split("\n\n")
+    .Select(x => x
+        .Split("\n", StringSplitOptions.RemoveEmptyEntries)
+        .Select(x => int.Parse(x))
+        .Aggregate((a, b) => a + b))
+    .Max();
+
+Console.WriteLine(result);
+
+
+
+
+string initialString = "1\n2\n3\n4\n\n5\n6";
+
+var maxOfSum = initialString
+    .Split("\n\n")
+    .Select(x => x
+        .Split("\n", StringSplitOptions.RemoveEmptyEntries)
+        .Select(x => int.Parse(x))
+        .Aggregate((a, b) => a + b))
+    .Max();
+
+Console.WriteLine(maxOfSum);
