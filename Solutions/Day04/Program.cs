1557d51f1c7b2d4664ab08bb512859c79c70ef4b
ElisendaGascon <elisendagascon@icloud.com>
2022-12-05 23:19:19 +0000

    Add day 4 part 2

--- Solutions/Day04/Program.cs
+++ Solutions/Day04/Program.cs
@@ -1,6 +1,8 @@
 ﻿string path = @"c:\Users\ElisendaGascon\repos\advent-of-code-2022\Solutions\Day04\Day04_Input.txt";
 string data = File.ReadAllText(path);
 
+// Part 1
+
 var result = data
     .Split(Environment.NewLine)
     .Select(x => new ValueTuple<string, string>(
@@ -11,27 +13,55 @@ var result1 = result
         (int.Parse(x.Item1.Split("-")[0]), int.Parse(x.Item1.Split("-")[1])),
         (int.Parse(x.Item2.Split("-")[0]), int.Parse(x.Item2.Split("-")[1]))));
 
-var result3 = IsFullyContained(result1) ;
+var result3 = IsFullyContained(result1);
 
 Console.WriteLine(result3);
 
 int IsFullyContained(IEnumerable<((int, int), (int, int))> assignments)
 {
-    int count = 0;
     int fullyContainedCount = 0;
 
     foreach (var assignment in assignments)
     {
-        count++;
         var a = assignment.Item1.Item1;
         var b = assignment.Item1.Item2;
         var c = assignment.Item2.Item1;
         var d = assignment.Item2.Item2;
 
-        if (a > c & b < d || a < c & b > d || a >= c & b < d || a > c & b <= d || a >= c & b <= d || a <= c & b > d || a < c & b >= d)
+        if (a > c & b < d 
+           || a < c & b > d
+           || a >= c & b < d
+           || a > c & b <= d
+           || a >= c & b <= d
+           || a <= c & b > d
+           || a < c & b >= d)
         {
             fullyContainedCount++;
         }
     }
     return fullyContainedCount;
+}
+
+// Part 2
+
+var result4 = Overlaps(result1);
+Console.WriteLine(result4);
+
+int Overlaps(IEnumerable<((int, int), (int, int))> assignments)
+{
+    int overlapsCount = 0;
+
+    foreach (var assignment in assignments)
+    {
+        var a = assignment.Item1.Item1;
+        var b = assignment.Item1.Item2;
+        var c = assignment.Item2.Item1;
+        var d = assignment.Item2.Item2;
+
+        if (a <= c & b >= c || d >= a & d <= b || a >= c & a <= d || b >= c & b <= d)
+        {
+            overlapsCount++;
+        }
+    }
+    return overlapsCount;
 }
